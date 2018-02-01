ecb37b9ef6dc8b6e8c8b346417be3c1e093fa936
Simon <au556327@post.au.dk>
2018-02-01 11:41:38 +0100

    Add Accumulator with Unit Tests

--- 1 Calculator/Calculator/Calculator.Test.Unit/CalculatorUnitTest.cs
+++ 1 Calculator/Calculator/Calculator.Test.Unit/CalculatorUnitTest.cs
@@ -54,7 +54,46 @@ namespace Calculator.Test.Unit
 	    {
 	        Assert.That(_uut.Power(x, exp), Is.EqualTo(result));
 	    }
-    }
+
+		// ---------------------------------------------------------------------
+		// Test Accumulator
+		[TestCase(2.3, 4, 6.3)]
+		[TestCase(-2, 4, 2)]
+		[TestCase(2, -4, -2)]
+		[TestCase(-2, -4, -6)]
+		public void Accumulator_AddPosAndNegNumbers_ResultIsCorrect(double a, double b, double result)
+		{
+			Assert.That(_uut.Add(a, b), Is.EqualTo(_uut.Accumulator));
+		}
+
+		[TestCase(2, 4, -2)]
+		[TestCase(-2, 4, -6)]
+		[TestCase(2, -4, 6)]
+		[TestCase(-2, -4, 2)]
+		public void Accumulator_SubtractPosAndNegNumbers_ResultIsCorrect(double a, double b, double result)
+		{
+			Assert.That(_uut.Subtract(a, b), Is.EqualTo(_uut.Accumulator));
+		}
+
+		[TestCase(2, 4, 8)]
+		[TestCase(-2, 4, -8)]
+		[TestCase(2, -4, -8)]
+		[TestCase(-2, -4, 8)]
+		[TestCase(0, 2, 0)]
+		[TestCase(2, 0, 0)]
+		[TestCase(0, 0, 0)]
+		public void Accumulator_MultiplyNumbers_ResultIsCorrect(double a, double b, double result)
+		{
+			Assert.That(_uut.Multiply(a, b), Is.EqualTo(_uut.Accumulator));
+		}
+
+		[TestCase(3, 2, 9)]
+		[TestCase(-3, 2, 9)]
+		public void Accumulator_RaiseNumbers_ResultIsCorrect(double x, double exp, double result)
+		{
+			Assert.That(_uut.Power(x, exp), Is.EqualTo(_uut.Accumulator));
+		}
+	}
 }
 
 namespace Calculator.Test.Unit
--- 1 Calculator/Calculator/Calculator/Calculator.cs
+++ 1 Calculator/Calculator/Calculator/Calculator.cs
@@ -8,24 +8,34 @@ namespace Calculator
 {
 	public class Calculator
 	{
+		public double Accumulator { get; set; }
+
 		public double Add(double a, double b)
 		{
-			return a + b;
+			Accumulator = a + b;
+
+			return Accumulator;
 		}
 
 		public double Subtract(double a, double b)
 		{
-			return a - b;
+			Accumulator = a - b;
+
+			return Accumulator;
 		}
 
 		public double Multiply(double a, double b)
 		{
-			return a * b;
+			Accumulator = a * b;
+
+			return Accumulator;
 		}
 
 		public double Power(double x, double exp)
 		{
-			return Math.Pow(x, exp);
+			Accumulator = Math.Pow(x, exp);
+
+			return Accumulator;
 		}
 	}	
 }
