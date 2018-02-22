73967b3c275f9f07ea89fb783a003b05f5c06c03
Simon <au556327@post.au.dk>
2018-02-22 13:13:10 +0100

    Small additions to UnitTest of ColorBet

--- 03_RouletteGame/RouletteGame.Test.Unit/ColorBetUnitTest.cs
+++ 03_RouletteGame/RouletteGame.Test.Unit/ColorBetUnitTest.cs
@@ -1,14 +1,21 @@
 ﻿using NUnit.Framework;
+using RouletteGame.Legacy;
 
 namespace RouletteGame.Test.Unit
 {
 	[TestFixture]
 	public class ColorBetUnitTest
 	{
+		private ColorBet _uut;
+
+		private string _name = "TestPerson";
+		private uint _amount = 100;
+		private uint _color = Field.Black;
+
 		[SetUp]
 		public void Init()
 		{
-
+			_uut = new ColorBet(_name, _amount, _color);
 		}
 	}
 }
