afb615b4f5129d79f51dfd4138cbdb8984ac595f
Katborg <s.katborg@gmail.com>
2018-02-23 20:55:20 +0100

    RouletteGame test done.

--- 03_RouletteGame/RouletteGame.Test.Unit/RouletteGameUnitTest.cs
+++ 03_RouletteGame/RouletteGame.Test.Unit/RouletteGameUnitTest.cs
@@ -4,86 +4,103 @@ using RouletteGame.Legacy;
 
 namespace RouletteGame.Test.Unit
 {
-	[TestFixture]
-	public class RouletteGameUnitTest
-	{
-	    private Legacy.RouletteGame _uut;
-	    private IRoulette _roulette;
-	    private IGameDisplay _display;
-	    private IBetHandler _betHandler;
+    [TestFixture]
+    public class RouletteGameUnitTest
+    {
+        private Legacy.RouletteGame _uut;
+        private IRoulette _roulette;
+        private IGameDisplay _display;
+        private IBetHandler _betHandler;
+
         [SetUp]
-		public void Init()
-		{
-		    _roulette = Substitute.For<IRoulette>();
+        public void Init()
+        {
+            _roulette = Substitute.For<IRoulette>();
             _display = Substitute.For<IGameDisplay>();
-		    _betHandler = Substitute.For<IBetHandler>();
+            _betHandler = Substitute.For<IBetHandler>();
 
-            _uut = new Legacy.RouletteGame(_roulette, _display, _betHandler );
-		}
+            _uut = new Legacy.RouletteGame(_roulette, _display, _betHandler);
+        }
 
-	    [Test]
-	    public void RouletteGame_display_OpenBets_Called()
-	    {
+        [Test]
+        public void RouletteGame_display_OpenBets_Called()
+        {
             //act
             _uut.OpenBets();
-            
-            //assert
-	        _display.Received().BetsOpen();
-	    }
-
-	    [Test]
-	    public void RouletteGame_display_ClosedBets_Called()
-	    {
-	        //act
-	        _uut.CloseBets();
 
             //assert
-	        _display.Received().BetsClosed();
+            _display.Received().BetsOpen();
+        }
+
+        [Test]
+        public void RouletteGame_display_ClosedBets_Called()
+        {
+            //act
+            _uut.CloseBets();
+
+            //assert
+            _display.Received().BetsClosed();
 
         }
 
-	    [Test]
-	    public void RouletteGame_display_PlacedBet_Roundclosed_ClosedBetsWarning_Called()
-	    {
+        [Test]
+        public void RouletteGame_display_PlacedBet_Roundclosed_ClosedBetsWarning_Called()
+        {
             ColorBet bet = new ColorBet("test", 100, 2);
 
-	        //act
-	        _uut.CloseBets();
+            //act
+            _uut.CloseBets();
             _uut.PlaceBet(bet);
 
-	        //assert
-	        _display.Received().BetsClosedWarning();
-
-	    }
-	    [Test]
-	    public void RouletteGame_display_PlacedBet_Roundopen_ClosedBetsWarning_NotCalled()
-	    {
-	        ColorBet bet = new ColorBet("test", 100, 2);
-
-	        //act
-	        _uut.OpenBets();
-	        _uut.PlaceBet(bet);
-
-	        //assert
-	        _display.DidNotReceive().BetsClosedWarning();
-
-	    }
-
-	    [Test]
-	    public void RouletteGame_display_SpinRoulette_Spin_called()
-	    {
-            _uut.SpinRoulette();
-	        _display.Received().Spin();
+            //assert
+            _display.Received().BetsClosedWarning();
 
         }
-	    [Test]
-	    public void RouletteGame_display_SpinRoulette_SpinResult_called()
-	    {
+
+        [Test]
+        public void RouletteGame_display_PlacedBet_Roundopen_ClosedBetsWarning_NotCalled()
+        {
+            ColorBet bet = new ColorBet("test", 100, 2);
+
+            //act
+            _uut.OpenBets();
+            _uut.PlaceBet(bet);
+
+            //assert
+            _display.DidNotReceive().BetsClosedWarning();
+
+        }
+
+        [Test]
+        public void RouletteGame_display_SpinRoulette_Spin_called()
+        {
+            _uut.SpinRoulette();
+            _display.Received().Spin();
+
+        }
+
+        [Test]
+        public void RouletteGame_display_SpinRoulette_SpinResult_called()
+        {
             Field field = new Field(1, 1);
 
-	        _uut.SpinRoulette();
-	        _display.ReceivedWithAnyArgs().SpinResult(field);
+            _uut.SpinRoulette();
+            var result = _roulette.GetResult();
 
-	    }
+            _display.ReceivedWithAnyArgs().SpinResult(result);
+
+        }
+
+        [Test]
+        public void RouletteGame_BetHandler_PayUp_called()
+        {
+            Field field = new Field(1, 1);
+
+            _uut.PayUp();
+            var result = _roulette.GetResult();
+            
+
+            _betHandler.Received().PayUp(result);
+        }
     }
 }
