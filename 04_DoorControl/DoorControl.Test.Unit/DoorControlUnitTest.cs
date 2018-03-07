0c4eb274283be673b7fc21304b892b4f357dec9e
Simon <au556327@post.au.dk>
2018-03-07 09:00:57 +0100

    Add Unit Tests for DoorControl

--- 04_DoorControl/DoorControl.Test.Unit/DoorControlUnitTest.cs
+++ 04_DoorControl/DoorControl.Test.Unit/DoorControlUnitTest.cs
@@ -22,5 +22,138 @@ namespace DoorControl.Test.Unit
 
 			_uut = new DoorControl(_fakeAlarm, _fakeDoor, _fakEntryNotification, _fakeUserValidation);
 		}
+
+		//------------------------------------------------------------------
+		// Test of Ctor
+		[Test]
+		public void Ctor_ForceOpenDoor_AlarmSignals()
+		{
+			_uut.DoorOpened();
+
+			_fakeAlarm.Received().SignalAlarm();
+		}
+
+		//------------------------------------------------------------------
+		// Test of RequestEntry()
+		[Test]
+		public void RequestEntry_DoorClosedStateAndValidationOK_OpenDoorIsCalled()
+		{
+			_fakeUserValidation.ValidateEntryRequest(Arg.Any<string>()).Returns(true);
+
+			_uut.RequestEntry("123");
+
+			_fakeDoor.Received().Open();
+		}
+
+		[Test]
+		public void RequestEntry_DoorClosedStateAndValidationOK_NotifyEntryGrantedIsCalled()
+		{
+			_fakeUserValidation.ValidateEntryRequest(Arg.Any<string>()).Returns(true);
+
+			_uut.RequestEntry("123");
+
+			_fakEntryNotification.Received().NotifyEntryGranted();
+		}
+
+		[Test]
+		public void RequestEntry_DoorClosedStateAndValidationNotOK_NotifyEntryDeniedIsCalled()
+		{
+			_fakeUserValidation.ValidateEntryRequest(Arg.Any<string>()).Returns(false);
+
+			_uut.RequestEntry("123");
+
+			_fakEntryNotification.Received().NotifyEntryDenied();
+		}
+
+		[Test]
+		public void RequestEntry_DoorClosedStateAndValidationOK_NotifyEntryDeniedIsNotCalled()
+		{
+			_fakeUserValidation.ValidateEntryRequest(Arg.Any<string>()).Returns(true);
+
+			_uut.RequestEntry("123");
+
+			_fakEntryNotification.DidNotReceive().NotifyEntryDenied();
+		}
+
+		[Test]
+		public void RequestEntry_DoorBreachedState_OpenDoorIsNotCalled()
+		{
+			// Bring system to breached state
+			_uut.DoorOpened();
+
+			_uut.RequestEntry("123");
+
+			_fakeDoor.DidNotReceive().Open();
+		}
+
+		[Test]
+		public void RequestEntry_DoorBreachedState_NotifyEntryDeniedIsNotCalled()
+		{
+			// Bring system to breached state
+			_uut.DoorOpened();
+
+			_uut.RequestEntry("123");
+
+			_fakEntryNotification.DidNotReceive().NotifyEntryDenied();
+		}
+
+		//------------------------------------------------------------------
+		// Test of DoorOpened()
+		[Test]
+		public void DoorOpened_DoorClosedState_SignalAlarmIsCalled()
+		{
+			_uut.DoorOpened();
+
+			_fakeAlarm.Received().SignalAlarm();
+		}
+
+		[Test]
+		public void DoorOpened_DoorClosedState_DoorCloseIsCalled()
+		{
+			_uut.DoorOpened();
+
+			_fakeDoor.Received().Close();
+		}
+
+		[Test]
+		public void DoorOpened_DoorOpeningState_DoorCloseIsCalled()
+		{
+			_fakeUserValidation.ValidateEntryRequest(Arg.Any<string>()).Returns(true);
+			_uut.RequestEntry("123");
+
+			_uut.DoorOpened();
+
+			_fakeDoor.Received().Close();
+		}
+
+		[Test]
+		public void DoorOpened_DoorOpeningState_SignalAlarmIsNotCalled()
+		{
+			_fakeUserValidation.ValidateEntryRequest(Arg.Any<string>()).Returns(true);
+			_uut.RequestEntry("123");
+
+			_uut.DoorOpened();
+
+			_fakeAlarm.DidNotReceive().SignalAlarm();
+		}
+
+		// We should now test that given the system is in another state,
+		// we should test that none of the above functions are called.
+		// We chose not to do so.
+
+		//------------------------------------------------------------------
+		// Test of DoorClosed()
+		[Test]
+		public void DoorClosed_DoorClosingState_SystemIsNowInDoorCloosedState()
+		{
+			_fakeUserValidation.ValidateEntryRequest(Arg.Any<string>()).Returns(true);
+			_uut.RequestEntry("123");
+			_uut.DoorOpened();
+
+			_uut.DoorClosed();
+
+			_uut.RequestEntry("123");
+			_fakeDoor.Received().Open();
+		}
 	}
 }
