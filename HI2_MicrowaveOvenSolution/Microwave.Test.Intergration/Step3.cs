0a5aecc36d11ee2f53769b8b4b600b0a03a97d99
Simon <au556327@gmail.com>
2018-03-16 10:49:09 +0100

    Add setup for test driver

--- HI2_MicrowaveOvenSolution/Microwave.Test.Intergration/Step3.cs
+++ HI2_MicrowaveOvenSolution/Microwave.Test.Intergration/Step3.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace Microwave.Test.Intergration
@@ -10,10 +9,46 @@ namespace Microwave.Test.Intergration
     [TestFixture]
     class Step3
     {
-        [SetUp]
-        public void Setup()
-        {
+	    // Classes that are under test
+	    private Button _powerButton;
+	    private Button _startCancelButton;
+	    private Button _timeButton;
+		private Door _door;
 
-        }
+	    // Stubs
+	    private IOutput _fakeDisplayOutput;
+	    private IOutput _fakePowerTubeOutput;
+	    private IOutput _fakeLightOutput;
+
+		// Classes that have been tested
+	    private Timer _timer;
+	    private Display _display;
+	    private PowerTube _powerTube;
+	    private Light _light;
+		private UserInterface _userInterface;
+	    private CookController _cookController;
+
+		[SetUp]
+        public void Setup()
+		{
+			_powerButton = new Button();
+			_startCancelButton = new Button();
+			_timeButton = new Button();
+			_door = new Door();
+
+			_fakeDisplayOutput = Substitute.For<IOutput>();
+			_fakePowerTubeOutput = Substitute.For<IOutput>();
+			_fakeLightOutput = Substitute.For<IOutput>();
+
+			_timer = new Timer();
+			_display = new Display(_fakeDisplayOutput);
+			_powerTube = new PowerTube(_fakePowerTubeOutput);
+			_light = new Light(_fakeLightOutput);
+			_userInterface = new UserInterface(_powerButton,
+				_timeButton, _startCancelButton, _door, _display,
+				_light, _cookController);
+			_cookController = new CookController(_timer, _display,
+				_powerTube, _userInterface);
+		}
     }
 }
