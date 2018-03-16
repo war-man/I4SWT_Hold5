f841eaa1b4350321e900f0cfafcf58ea739fafe9
Katborg <s.katborg@gmail.com>
2018-03-16 11:12:55 +0100

    Step1 done

--- HI2_MicrowaveOvenSolution/Microwave.Test.Intergration/Step1.cs
+++ HI2_MicrowaveOvenSolution/Microwave.Test.Intergration/Step1.cs
@@ -1,44 +1,71 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Microwave
 using MicrowaveOvenClasses.Boundary;
 using MicrowaveOvenClasses.Controllers;
 using MicrowaveOvenClasses.Interfaces;
 using NSubstitute;
 using NUnit.Framework;
 using NUnit.Framework.Internal.Commands;
+using Assert = NUnit.Framework.Assert;
 
 namespace Microwave.Test.Intergration
 {
-    [TestFixture]
-    public class step1
-    {
-        private IOutput _output;
-        private IDisplay _display;
-        private ITimer _timer;
-        private IPowerTube _powerTube;
-        private ICookController _cookController;
-        [SetUp]
-        public void Setup()
-        {
-            _output = Substitute.For<IOutput>();
-            _display = new Display(_output);
-            _timer = new Timer();
-            _powerTube = new PowerTube(_output);
-            _cookController = new CookController(_timer,_display,_powerTube);
+	[TestFixture]
+	public class step1
+	{
+		private IOutput _outputDisplay;
+		private IOutput _outputPower;
 
-        }
+		private IDisplay _display;
+		private ITimer _timer;
+		private IPowerTube _powerTube;
+		private ICookController _cookController;
+		[SetUp]
+		public void Setup()
+		{
+			_outputDisplay = Substitute.For<IOutput>();
+			_outputPower = Substitute.For<IOutput>();
+			_display = new Display(_outputDisplay);
+			_timer = new Timer();
+			_powerTube = new PowerTube(_outputPower);
+			_cookController = new CookController(_timer, _display, _powerTube);
 
-        [Test]
-        public void CookController_StartCooking_OutputRecieved()
-        {
-            //Arrange
+		}
 
-            //Act
-            _cookController.StartCooking(5, 5);
-            //Assert
-            _output.Received().OutputLine(Arg.Any<string>());
-        }
+		[Test]
+		public void CookController_StartCooking_OutputPowerRecieved()
+		{
+			//Arrange
+			int timesCalled = 0;
+			//Act
 
-    }
+			_outputPower.When(x=> x.OutputLine(Arg.Any<string>()))
+				.Do(x => ++timesCalled);
+
+			_cookController.StartCooking(50, 2000);
+			
+			//Assert
+			Assert.That(() => (timesCalled == 2), Is.True.After(2500));
+		}
+
+
+		[Test]
+		public void CookController_StartCooking_OutputDisplayRecievedFiveTimes()
+		{
+			//Arrange
+			int timesCalled = 0;
+			//Act
+			_cookController.StartCooking(50, 5000);
+			
+			_outputDisplay.When(x => x.OutputLine(Arg.Any<string>()))
+				.Do(x => ++timesCalled);
+
+			//Assert
+			Assert.That(() => (timesCalled == 5), Is.True.After(5500));
+
+
+		}
+		
+
+	}
 }
