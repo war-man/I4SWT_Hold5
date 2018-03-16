3aae5943dc72af2e0fc7a7c520a8392273c6497d
Katborg <s.katborg@gmail.com>
2018-03-16 09:29:40 +0100

    Setup files for steps.

---
+++ HI2_MicrowaveOvenSolution/Microwave.Test.Intergration/Step1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microwave
+using MicrowaveOvenClasses.Interfaces;
+using NUnit.Framework;
+
+namespace Microwave.Test.Intergration
+{
+    [TestFixture]
+    public class step1
+    {
+        private IOutput _output
+        private IDisplay _display;
+        private ITimer _timer;
+        private IPowerTube _powerTube;
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [Test]
+        public void TurnOn_WasOff_CorrectOutput()
+        {
+            
+        }
+
+    }
+}
