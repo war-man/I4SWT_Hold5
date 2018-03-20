a77fb7f3eede522a1f1e8f9adf843cb9bf93ed92
Katborg <s.katborg@gmail.com>
2018-03-20 19:45:42 +0100

    made a application that uses microovnen

--- HI2_MicrowaveOvenSolution/Microwave.Application/MicrowaveApplication.cs
+++ HI2_MicrowaveOvenSolution/Microwave.Application/MicrowaveApplication.cs
@@ -36,6 +36,42 @@ namespace Microwave.Application
 
 			cookController.UI = userInterface;
 
+
+			//Sumulationg user activities
+			Console.WriteLine("User opens door, puts in food and closes the door");
+			door.Open();
+			door.Close();
+
+			Console.WriteLine("User set up power and time");
+			for (int i = 0; i < 5; i++)
+			{
+			powerButton.Press();	
+			}
+
+			for (int i = 0; i < 1; i++)
+			{
+				timeButton.Press();
+			}
+
+			Console.WriteLine("User start the microwave oven and stands there looking at his food turning around");
+			startCancelButton.Press();
+
+			System.Threading.Thread.Sleep(10000);
+
+			Console.WriteLine("User want to check if food is ready so opens the door");
+			door.Open();
+			Console.WriteLine("Suprisingly, food was not ready, User closes dooor");
+			door.Close();
+
+			Console.WriteLine("User presses power and time button");
+			for (int i = 0; i < 10; i++)
+			{
+				powerButton.Press();
+			}
+			timeButton.Press();
+			Console.WriteLine("user starts the microwave oven agein");
+			startCancelButton.Press();
+			
 			// Wait while the classes, including the timer, do their job
 			System.Console.WriteLine("Press any button to close the application.");
 			System.Console.ReadLine();
