79f1e7df00dc02a444d8cea0bb24c73a67ab4486
Katborg <s.katborg@gmail.com>
2018-03-20 14:10:07 +0100

    Timer. Change return time to sek

--- HI2_MicrowaveOvenSolution/MicrowaveOvenClasses/Controllers/CookController.cs
+++ HI2_MicrowaveOvenSolution/MicrowaveOvenClasses/Controllers/CookController.cs
@@ -64,7 +64,7 @@ namespace MicrowaveOvenClasses.Controllers
 
         public void OnTimerTick(object sender, EventArgs e)
         {
-            int remaining = myTimer.TimeRemaining;
+            int remaining = myTimer.TimeRemaining /1000;
             myDisplay.ShowTime(remaining/60, remaining % 60);
         }
     }
