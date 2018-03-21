58ee1184525ea281c32a6d62f71eb84ec583fbff
Djonn <djonnneller@gmail.com>
2018-03-21 18:05:37 +0100

    Edited and added integration tests for step4

--- HI2_MicrowaveOvenSolution/Microwave.Test.Intergration/Step4.cs
+++ HI2_MicrowaveOvenSolution/Microwave.Test.Intergration/Step4.cs
@@ -13,196 +13,114 @@ using NUnit.Framework;
 
 namespace Microwave.Test.Intergration
 {
-    [TestFixture]
-    class Step4
-    {
-	    private IOutput _output;
-	    
+	[TestFixture]
+	class Step4
+	{
+		#region Setup
+		private IOutput _output;
+		private IButton _powerButton;
+		private IButton _timeButton;
+		private IButton _startButton;
+		private IDoor _door;
+		private IDisplay _display;
+		private ICookController _cookController;
+		private IUserInterface _userInterface;
 
-	    private IButton _powerButton;
-	    private IButton _timerButton;
-	    private IButton _startButton;
-
-	    private IDoor _door;
-
-	    private IUserInterface _userInterface;
-
-	    private ConsoleOutput _consoleOutput;
+		private ConsoleOutput _consoleOutput;
 
 		[SetUp]
-        public void Setup()
-        {
-	        _consoleOutput = new ConsoleOutput();
+		public void Setup()
+		{
+			_consoleOutput = new ConsoleOutput();
 
 			_powerButton = new Button();
-	        _timerButton = new Button();
+			_timeButton = new Button();
 			_startButton = new Button();
+			_output = new Output();
 
-	        _output = new Output();
+			_display = new Display(_output);
+			_cookController = new CookController(new Timer(), _display, new PowerTube(_output));
 
-			IDisplay display = new Display(_output);
-	        var cookController = new CookController(new Timer(), display, new PowerTube(_output));
+			_userInterface = new UserInterface(_powerButton, _timeButton, _startButton, _door = new Door(), _display, new Light(_output), _cookController);
+			((CookController)(_cookController)).UI = _userInterface;
 
-	        _userInterface = new UserInterface(_powerButton,_timerButton,_startButton, _door = new Door(), display, new Light(_output), cookController);
-
-	        cookController.UI = _userInterface;
-
-        }
-
-	    [Test]
-	    public void output_openDoor_OutputCorrect()
-	    {
-			//Arrange
-			
-			//Act
-			_door.Open();
-			
-			//Assert
-			Assert.IsTrue(_consoleOutput.GetOuput().Contains("Light") && _consoleOutput.GetOuput().Contains("turned on"));
 		}
-	    [Test]
-	    public void output_closeDoor_OutputCorrect()
-	    {
-		    //Arrange
-			_door.Open();
 
-		    //Act
-		    _door.Close();
+		#endregion
 
-			//Assert
-		    Assert.IsTrue(_consoleOutput.GetOuput().Contains("Light") && _consoleOutput.GetOuput().Contains("turned off"));
-	    }
-
-	    [TestCase(1)]
-	    [TestCase(5)] // ikke nødvendigt at gøre andet end en gang. 
-		public void output_PowerbuttonPress_OutputCorrect(int timesToPress)
-	    {
-		    //Arrange
-		   
-
-			//Act
-		    for (int i = 0; i < timesToPress; i++)
-		    {
-			    _powerButton.Press();
-		    }
-
-		    //Assert
-			Assert.IsTrue(_consoleOutput.GetOuput().Contains("Display") && _consoleOutput.GetOuput().Contains( $"{timesToPress * 50} W"));
-	    }
-
-		[TestCase(1)]
-		[TestCase(5)] // ikke nødvendigt at gøre andet end en gang. 
-		public void output_timebuttonPress_inPowerState_OutputCorrect(int timesToPress)
-	    {
-		    //Arrange
-			_powerButton.Press();
-			//Act
-			for (int i = 0; i < timesToPress; i++)
-			{
-				_timerButton.Press();
-			}
-			//Assert
-			Assert.IsTrue(_consoleOutput.GetOuput().Contains("Display") && _consoleOutput.GetOuput().Contains($"{timesToPress:D2}:00"));
-	    }
-
-	    [TestCase(1)]
-	    [TestCase(5)]
-	    public void output_timebuttonPress_notInPowerstate_OutputCorrect(int timesToPress)
-	    {
-		    //Arrange
-		    //Act
-		    for (int i = 0; i < timesToPress; i++)
-		    {
-			    _timerButton.Press();
-		    }
-		    //Assert
-		    Assert.IsFalse(_consoleOutput.GetOuput().Contains("Display") && _consoleOutput.GetOuput().Contains($"{timesToPress:D2}:00"));
-	    }
-
-	    [TestCase(1, 1)]
-	    [TestCase(1, 2)]
-	    [TestCase(1, 3)]
-		public void output_CookingState_PowerOutput(int timesPressed, int powerPressed)
+		#region Display -> Output
+		[Test]
+		public void DisplayOutput_LogLine_OutputWritesDisplayToConsole()
 		{
 			//Arrange
-			string output = null;
-			for (int i = 0; i < powerPressed; i++)
-			{
-				_powerButton.Press();
-			}
-			for (int i = 0; i < timesPressed; i++)
-			{
-				_timerButton.Press();
-			}
-			
+
 			//Act
-			_startButton.Press();
-			output = _consoleOutput.GetOuput();
+			_powerButton.Press();
 
-			double procent = (powerPressed*50.0 / 700.0) * 100.0;
-			
 			//Assert
-			Assert.IsTrue(output.Contains("PowerTube") && output.Contains($"{(int)procent} %"));
-
+			Assert.IsTrue(_consoleOutput.GetOuput().Contains($"Display"));
 		}
 
-	    [TestCase(2, 1, 1)]
-		[TestCase(2, 1, 10)] // ikke nødvendigt at gøre andet end en gang. 
-		public void output_CookingState_TimeOutput(int timesPressed, int powerPressed, int testAfterTime)
-	    {
+		#endregion
+
+		#region PowerTube -> Output
+		[Test]
+		public void PowerTubeOutput_LogLine_OutputWritesPowerTubeToConsole()
+		{
 			//Arrange
-		    int timescalc = (timesPressed * 60) - testAfterTime;
-		    string output = String.Empty;
+			_powerButton.Press();
+			_timeButton.Press();
 
-		    for (int i = 0; i < powerPressed; i++)
-		    {
-			    _powerButton.Press();
-			   
-		    }
-		    for (int i = 0; i < timesPressed; i++)
-		    {
-			    _timerButton.Press();
-			   
-		    }
 			//Act
 			_startButton.Press();
 
-
-
 			//Assert
-			Assert.That( () =>
-					//taknes all console output and splits into an array of strings. Reverse it so that the lastline is in top, keeps the "last two" and then check the last non empty line
-					_consoleOutput.GetOuput().Split('\n').Reverse().Take(2).ToArray()[1].Contains("Display") &&
-					_consoleOutput.GetOuput().Split('\n').Reverse().Take(2).ToArray()[1].Contains($"{(timescalc/60):D2}:{(timescalc % 60):D2}"),
-						Is.True.After(testAfterTime * 1000  + 500));
-
+			Assert.IsTrue(_consoleOutput.GetOuput().Contains($"PowerTube"));
 		}
 
-		//helper class
+		#endregion
+
+		#region Light -> Output
+		[Test]
+		public void LightOutput_LogLine_OutputWritesLightToConsole()
+		{
+			//Arrange
+
+			//Act
+			_door.Open();
+
+			//Assert
+			Assert.IsTrue(_consoleOutput.GetOuput().Contains($"Light"));
+		}
+
+		#endregion
+
+		#region Utilities
 		public class ConsoleOutput : IDisposable
-	    {
+		{
 			//From https://stackoverflow.com/questions/2139274/grabbing-the-output-sent-to-console-out-from-within-a-unit-test
 
-		    private StringWriter stringWriter;
-		    private TextWriter originalOutput;
+			private StringWriter stringWriter;
+			private TextWriter originalOutput;
 
-		    public ConsoleOutput()
-		    {
-			    stringWriter = new StringWriter();
-			    originalOutput = Console.Out;
-			    Console.SetOut(stringWriter);
-		    }
+			public ConsoleOutput()
+			{
+				stringWriter = new StringWriter();
+				originalOutput = Console.Out;
+				Console.SetOut(stringWriter);
+			}
 
-		    public string GetOuput()
-		    {
-			    return stringWriter.ToString();
-		    }
+			public string GetOuput()
+			{
+				return stringWriter.ToString();
+			}
 
-		    public void Dispose()
-		    {
-			    Console.SetOut(originalOutput);
-			    stringWriter.Dispose();
-		    }
-	    }
+			public void Dispose()
+			{
+				Console.SetOut(originalOutput);
+				stringWriter.Dispose();
+			}
+		}
+		#endregion
 	}
 }
