b5e934beac42c87e5aa0f18e1ce51cfd59238a70
Simon <au556327@gmail.com>
2018-03-21 17:11:01 +0100

    Correct some grammar errors in test-application

--- HI2_MicrowaveOvenSolution/Microwave.Application/MicrowaveApplication.cs
+++ HI2_MicrowaveOvenSolution/Microwave.Application/MicrowaveApplication.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using MicrowaveOvenClasses.Boundary;
 using MicrowaveOvenClasses.Controllers;
-using MicrowaveOvenClasses.Interfaces;
 
 namespace Microwave.Application
 {
@@ -25,56 +20,60 @@ namespace Microwave.Application
 				new Timer(),
 				new Display(output),
 				new PowerTube(output));
+
 			var userInterface = new UserInterface(
 				powerButton,
 				timeButton,
 				startCancelButton,
-				door, 
+				door,
 				new Display(output),
-				new Light(output), 
+				new Light(output),
 				cookController);
 
 			cookController.UI = userInterface;
 
 
-			//Sumulationg user activities
-			Console.WriteLine("User opens door, puts in food and closes the door");
+			// Simulation of user activities
+			Console.WriteLine("User opens the door.");
 			door.Open();
+
+			Console.WriteLine("User puts in his food and closes the door.");
 			door.Close();
 
-			Console.WriteLine("User set up power and time");
+			Console.WriteLine("User sets up power level by pressing the power button a couple of times.");
 			for (int i = 0; i < 5; i++)
 			{
-			powerButton.Press();	
+				powerButton.Press();
 			}
 
-			for (int i = 0; i < 1; i++)
-			{
-				timeButton.Press();
-			}
+			Console.WriteLine("User sets up timer.");
+			timeButton.Press();
 
-			Console.WriteLine("User start the microwave oven and stands there looking at his food turning around");
+			Console.WriteLine("User starts the microwave oven and stands there looking at his food turning around.");
 			startCancelButton.Press();
-
 			System.Threading.Thread.Sleep(10000);
 
-			Console.WriteLine("User want to check if food is ready so opens the door");
+			Console.WriteLine("User wants to check if food is ready, so he opens the door.");
 			door.Open();
-			Console.WriteLine("Suprisingly, food was not ready, User closes dooor");
+
+			Console.WriteLine("Surprisingly, the food was not ready. User closes door again.");
 			door.Close();
 
-			Console.WriteLine("User presses power and time button");
+			Console.WriteLine("User sets up power level by pressing the power button a couple of times.");
 			for (int i = 0; i < 10; i++)
 			{
 				powerButton.Press();
 			}
+
+			Console.WriteLine("User sets up timer.");
 			timeButton.Press();
-			Console.WriteLine("user starts the microwave oven agein");
+
+			Console.WriteLine("User starts the microwave oven again.");
 			startCancelButton.Press();
-			
+
 			// Wait while the classes, including the timer, do their job
-			System.Console.WriteLine("Press any button to close the application.");
-			System.Console.ReadLine();
+			Console.WriteLine("Press any button to close the application.");
+			Console.ReadLine();
 		}
 	}
 }
