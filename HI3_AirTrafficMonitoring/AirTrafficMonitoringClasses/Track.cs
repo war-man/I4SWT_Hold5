4142e5bc1a96899ce9676d03aacd7aa7410c45ed
Katborg <s.katborg@gmail.com>
2018-04-05 10:53:08 +0200

    made track class

---
+++ HI3_AirTrafficMonitoring/AirTrafficMonitoringClasses/Track.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirTrafficMonitoringClasses
+{
+	class Track
+	{
+		public string Tag { get; set; }
+		public string XCoordinate { get; set; }
+
+		public string YCoordinate { get; set; }
+		public string Altitude { get; set; }
+		public string Timestamp { get; set; }
+	}
+
+	
+}
