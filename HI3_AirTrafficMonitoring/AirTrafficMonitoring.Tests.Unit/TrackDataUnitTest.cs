46e5eba684bc2da2cf2becd43abab9177bdf84db
au556327 <au556327@gmail.com>
2018-04-07 14:06:46 +0200

    Add empty class for TrackData

---
+++ HI3_AirTrafficMonitoring/AirTrafficMonitoring.Tests.Unit/TrackDataUnitTest.cs
@@ -0,0 +1,17 @@
+﻿using AirTrafficMonitoring.Classes.TrackDataModels;
+using NUnit.Framework;
+
+namespace AirTrafficMonitoring.Tests.Unit
+{
+	[TestFixture]
+	class TrackDataUnitTest
+	{
+		private TrackData _uut;
+
+		[SetUp]
+		public void Init()
+		{
+			_uut = new TrackData();
+		}
+	}
+}
