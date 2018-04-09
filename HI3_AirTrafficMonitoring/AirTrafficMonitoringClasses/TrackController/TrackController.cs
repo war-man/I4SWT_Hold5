6b8fdacff02644931ea21461e28712d032d19e65
Djonn <djonnneller@gmail.com>
2018-04-09 13:15:50 +0200

    Updated TrackController to work with new ConsolePrinter

--- HI3_AirTrafficMonitoring/AirTrafficMonitoringClasses/TrackController/TrackController.cs
+++ HI3_AirTrafficMonitoring/AirTrafficMonitoringClasses/TrackController/TrackController.cs
@@ -1,25 +1,28 @@
 ﻿using AirTrafficMonitoring.Classes.CurrentTracksManager;
 using AirTrafficMonitoring.Classes.TrackDataModels;
 using AirTrafficMonitoring.Classes.TrackGenerator;
-using AirTrafficMonitoring.Classes.TrackPrinter;
 using System.Collections.Generic;
+using AirTrafficMonitoring.Classes.Printer;
 
 namespace AirTrafficMonitoring.Classes.TrackController
 {
 	public class TrackController : ITrackController
 	{
 		private readonly ITrackGenerator _trackGenerator;
-		private readonly ITrackPrinter _trackPrinter;
+		private readonly ITrackListFormatter _formatter;
+		private readonly IPrinter _printer;
 		private readonly ICurrentTracksManager _currentTracksManager;
 
 		public TrackController(
 			ICurrentTracksManager currentTracksManager,
 			ITrackGenerator trackGenerator,
-			ITrackPrinter trackPrinter)
+			ITrackListFormatter formatter,
+			IPrinter printer)
 		{
 			_currentTracksManager = currentTracksManager;
 			_trackGenerator = trackGenerator;
-			_trackPrinter = trackPrinter;
+			_formatter = formatter;
+			_printer = printer;
 		}
 
 		public void AddTrackDataObjects(List<TrackData> trackDataList)
@@ -60,7 +63,13 @@ namespace AirTrafficMonitoring.Classes.TrackController
 			}
 
 			if (_currentTracksManager.GetTrackCount() > 0)
-				_trackPrinter.Print(_currentTracksManager.CurrentTracks);
+			{
+				string formattedString = _formatter.Format(_currentTracksManager.CurrentTracks);
+
+				_printer.Clear();
+				_printer.WriteLine(formattedString);
+			}
+				
 		}
 	}
 }
