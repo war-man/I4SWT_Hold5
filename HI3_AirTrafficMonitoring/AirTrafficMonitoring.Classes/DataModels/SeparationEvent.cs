de6e10cbcdb78478b507ed96b7f6c5b052fa0a24
au556327 <au556327@gmail.com>
2018-04-17 10:02:26 +0200

    Add SeparationEvent data object

--- HI3_AirTrafficMonitoring/AirTrafficMonitoring.Classes/DataModels/SeparationEvent.cs
+++ HI3_AirTrafficMonitoring/AirTrafficMonitoring.Classes/DataModels/SeparationEvent.cs
@@ -4,8 +4,20 @@ namespace AirTrafficMonitoring.Classes.DataModels
 {
 	public class SeparationEvent
 	{
+		public SeparationEvent(string tag1, string tag2, DateTime timestamp)
+		{
+			Tag1 = tag1;
+			Tag2 = tag2;
+			Timestamp = timestamp;
+		}
+
 		public string Tag1 { get; set; }
 		public string Tag2 { get; set; }
 		public DateTime Timestamp { get; set; }
+
+		public override string ToString()
+		{
+			return $"Track 1: {Tag1} | Track 2: {Tag2} | Timestamp: {Timestamp}";
+		}
 	}
 }
