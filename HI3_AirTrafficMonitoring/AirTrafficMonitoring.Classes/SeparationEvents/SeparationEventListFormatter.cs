768e52280da39487fbb03f296ac564054f32613f
au556327 <au556327@gmail.com>
2018-04-17 09:59:49 +0200

    Add SeparationEventListFormatter

---
+++ HI3_AirTrafficMonitoring/AirTrafficMonitoring.Classes/SeparationEvents/SeparationEventListFormatter.cs
@@ -0,0 +1,24 @@
+﻿using AirTrafficMonitoring.Classes.DataModels;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AirTrafficMonitoring.Classes.SeparationEvents
+{
+	public class SeparationEventListFormatter : ISeparationEventListFormatter
+	{
+		public string Format(List<SeparationEvent> separationEventList)
+		{
+			if (separationEventList.Count == 0) return "";
+
+			var returnString = new StringBuilder();
+
+			returnString.AppendLine($"Current number of separation events: {separationEventList.Count}.\n");
+			foreach (var separationEvent in separationEventList)
+			{
+				returnString.AppendLine(separationEvent + "\n");
+			}
+
+			return returnString.ToString();
+		}
+	}
+}
