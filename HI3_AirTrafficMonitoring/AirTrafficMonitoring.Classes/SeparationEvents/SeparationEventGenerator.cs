cf8dc7b551e517df4fcb6177fcc399c5bcb9b1e1
au556327 <au556327@gmail.com>
2018-04-17 09:59:34 +0200

    Add SeparationEventGenerator

---
+++ HI3_AirTrafficMonitoring/AirTrafficMonitoring.Classes/SeparationEvents/SeparationEventGenerator.cs
@@ -0,0 +1,15 @@
+﻿using AirTrafficMonitoring.Classes.DataModels;
+using System;
+
+namespace AirTrafficMonitoring.Classes.SeparationEvents
+{
+	public class SeparationEventGenerator : ISeparationEventGenerator
+	{
+		public SeparationEvent GenerateSeparationEvent(Track track1, Track track2, DateTime timestamp)
+		{
+			if (track1 == null || track2 == null || timestamp == default(DateTime)) return null;
+
+			return new SeparationEvent(track1.Tag, track2.Tag, timestamp);
+		}
+	}
+}
