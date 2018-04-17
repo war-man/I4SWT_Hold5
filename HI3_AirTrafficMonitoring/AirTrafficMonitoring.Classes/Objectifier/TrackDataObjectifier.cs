de1c02acac74df57b1d9c9747b5eddbbe83c7764
au556327 <au556327@gmail.com>
2018-04-17 10:04:23 +0200

    Update timestamp element

--- HI3_AirTrafficMonitoring/AirTrafficMonitoring.Classes/Objectifier/TrackDataObjectifier.cs
+++ HI3_AirTrafficMonitoring/AirTrafficMonitoring.Classes/Objectifier/TrackDataObjectifier.cs
@@ -18,16 +18,10 @@ namespace AirTrafficMonitoring.Classes.Objectifier
 				var xCoordinate = int.Parse(dataElements[1]);
 				var yCoordinate = int.Parse(dataElements[2]);
 				var altitude = int.Parse(dataElements[3]);
-				var year = int.Parse(dataElements[4].Substring(0, 4));
-				var month = int.Parse(dataElements[4].Substring(4, 2));
-				var day = int.Parse(dataElements[4].Substring(6, 2));
-				var hour = int.Parse(dataElements[4].Substring(8, 2));
-				var minute = int.Parse(dataElements[4].Substring(10, 2));
-				var seconds = int.Parse(dataElements[4].Substring(12, 2));
-				var milliseconds = int.Parse(dataElements[4].Substring(14, 3));
-				var date = new DateTime(year, month, day, hour, minute, seconds, milliseconds);
+				var timestamp = DateTime.ParseExact(dataElements[4], "yyyyMMddHHmmssfff",
+					System.Globalization.CultureInfo.InvariantCulture);
 
-				trackDataList.Add(new TrackData(tag, xCoordinate, yCoordinate, altitude, date));
+				trackDataList.Add(new TrackData(tag, xCoordinate, yCoordinate, altitude, timestamp));
 			}
 
 			return trackDataList;
