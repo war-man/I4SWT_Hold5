32316d3804e912b50ff87af036e970d1a9cf98fe
au556327 <au556327@gmail.com>
2018-04-17 10:07:44 +0200

    Add implementation of SeparationEventController

--- HI3_AirTrafficMonitoring/AirTrafficMonitoring.Classes/SeparationEvents/ISeparationEventController.cs
+++ HI3_AirTrafficMonitoring/AirTrafficMonitoring.Classes/SeparationEvents/ISeparationEventController.cs
@@ -1,6 +1,11 @@
-﻿namespace AirTrafficMonitoring.Classes.SeparationEvents
+﻿using AirTrafficMonitoring.Classes.DataModels;
+using System.Collections.Generic;
+
+namespace AirTrafficMonitoring.Classes.SeparationEvents
 {
 	public interface ISeparationEventController
 	{
+		List<SeparationEvent> CheckForSeparationEvents(List<Track> trackList);
+		string GetFormattedSeparationEvents();
 	}
 }
