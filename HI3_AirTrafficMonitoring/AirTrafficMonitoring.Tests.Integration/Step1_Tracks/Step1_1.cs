2485b557d680de9e950af0dbea7c867bf0efa467
au556327 <au556327@gmail.com>
2018-04-23 14:22:21 +0200

    Add Integration Test for new Step1_1

---
+++ HI3_AirTrafficMonitoring/AirTrafficMonitoring.Tests.Integration/Step1_Tracks/Step1_1.cs
@@ -0,0 +1,45 @@
+﻿using AirTrafficMonitoring.Classes.DataModels;
+using AirTrafficMonitoring.Classes.Tracks;
+using NUnit.Framework;
+using System;
+
+namespace AirTrafficMonitoring.Tests.Integration.Step1_Tracks
+{
+	class Step1_1
+	{
+		private ICurrentTracksManager _currentTracksManager;
+
+		// Boundaries for monitored area
+		private const int XBoundarySouthWest = 10000;
+		private const int YBoundarySouthWest = 10000;
+		private const int XBoundaryNorthEast = 90000;
+		private const int YBoundaryNorthEast = 90000;
+		private const int ZBoundaryLower = 500;
+		private const int ZBoundaryUpper = 20000;
+
+		[SetUp]
+		public void Init()
+		{
+			_currentTracksManager = new CurrentTracksManager();
+		}
+
+		[Test]
+		public void UpdateTrack_CallUpdateTrackWithNewTrackData_CurrentTrackOnActualTrackIsSetCorrectly()
+		{
+			// Arrange
+			const string tag = "someTag";
+
+			var originalTrackDataObject = new TrackData(tag, XBoundarySouthWest, YBoundarySouthWest, ZBoundaryLower, DateTime.Now);
+			_currentTracksManager.AddTrack(new Track(tag, originalTrackDataObject));
+
+			var newTrackDataObject = new TrackData(tag, XBoundaryNorthEast, YBoundaryNorthEast, ZBoundaryUpper, DateTime.Now);
+
+			// Act
+			_currentTracksManager.UpdateTrack(newTrackDataObject);
+
+			// Assert
+			var track = _currentTracksManager.FindTrack(tag);
+			Assert.That(track.CurrentTrack, Is.EqualTo(newTrackDataObject));
+		}
+	}
+}
